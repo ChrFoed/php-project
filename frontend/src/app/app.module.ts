caaea137bc9056e41b3ebcc239cfa443639398c8
Christian Födermayr <christian.foedermayr@gmail.com>
2021-06-28 22:20:53 +0200

    inital commit - docker-compose and bare laravel, postgres and angular projects

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingpageComponent,
+    ProductsComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
