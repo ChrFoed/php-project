6ee236963d9b2b856427f1128535a5ae3e6c0c5f
Christian Födermayr <christian.foedermayr@gmail.com>
2021-07-01 10:27:00 +0200

    make script executable

--- buildApp.sh
+++ buildApp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Note: If you install docker & docker-compose for the right user - delete sudo
-
+sudo docker-compose down
 ## Install vendor files
 composer install --working-dir=backend
 ## build docker environments
