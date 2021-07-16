08ed04b52b21aff1a1bbaf4d194c210deac2dfd0
Christian Födermayr <christian.foedermayr@gmail.com>
2021-07-16 10:15:55 +0200

    better User Agent

--- backend/app/Http/Controllers/ScrapingController.php
+++ backend/app/Http/Controllers/ScrapingController.php
@@ -53,7 +53,7 @@ class ScrapingController extends Controller
         #$client = new Client(HttpClient::create(['proxy' => $proxy, 'timeout' => 60]));
         $client = new Client(HttpClient::create(['timeout' => 60]));
         $client->followRedirects();
-        $client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
+        $client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36');
         $client->setServerParameter('HTTP_ACCEPT_LANGUAGE', 'de-DE');
         $client->setServerParameter('HTTP_REFERER', 'https://www.google.com/');
         return $client;
